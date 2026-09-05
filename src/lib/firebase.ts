41d5c287f9e43adba564436efc66dc70169da7b4
Dr M Irfan Qadir Thaheem <dr.mirfan5577@gmail.com>
2026-09-05 21:14:16 +0500

    Update firebase.ts
    Fix Firebase config with correct API Key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -61,3 +61,13 @@ export async function verifyPhoneOTP(otp: string): Promise<any> {
     throw error;
   }
 }
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBirtqTZoybU2z_d3OEr_TZshb19dyyXZo",
+  authDomain: "unifeel--esoneworld.firebaseapp.com",
+  projectId: "unifeel--esoneworld",
+  storageBucket: "unifeel--esoneworld.firebasestorage.app",
+  messagingSenderId: "801094166684",
+  appId: "1:801094166684:web:c9ea55ffe4b964ad23a559"
+};
